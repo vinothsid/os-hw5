89d42d972e4181468d63ee2b69c992efaf58e4b2
Vinoth <vinothsid@gmail.com>
2012-03-16 23:27:05 -0400

    modified metadata structure

--- log_fs.h
+++ log_fs.h
@@ -1,11 +1,12 @@
 #define MAX_FILE_NAME_SIZE 100
 #define MAX_PATH_NAME_SIZE 100
+#define MAX_PATHS 1000
 #define LOG_FS_H 1
 
 typedef struct lfs_meta_data {
 	char file_name[MAX_FILE_NAME_SIZE];
-	int num_blocks;
-	char path[MAX_PATH_NAME_SIZE];
+	int num_paths;
+	char path[MAX_PATHS][MAX_PATH_NAME_SIZE];
 } MDATA;
 
 	
